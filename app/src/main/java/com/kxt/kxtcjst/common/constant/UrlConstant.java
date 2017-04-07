d43d6e9da9738f97ba8e09fcdd88e5c152e0820b
Android-罗志敏 <54543534@domain.com>
2017-04-07 13:04:44 +0800

    线上接口ok1111111111111

--- app/src/main/java/com/kxt/kxtcjst/common/constant/UrlConstant.java
+++ app/src/main/java/com/kxt/kxtcjst/common/constant/UrlConstant.java
@@ -5,13 +5,13 @@ package com.kxt.kxtcjst.common.constant;
  */
 
 public class UrlConstant {
-//    public static final String BASE_URL = "http://adi.kuaixun56.com/";
-public static final String BASE_URL = "http://test.adi.kuaixun56.com/";
+    public static final String BASE_URL = "http://adi.kuaixun56.com/";
+//public static final String BASE_URL = "http://test.adi.kuaixun56.com/";
     public static final String SYSTEM_VALUE = "android";
 
 
-    public static final String UPDATE_MSG_URL = BASE_URL + "app/pkx/version";//更新信息
-    public static final String AD_CONFIG_URL = BASE_URL + "app/pkx/config";//广告信息
+    public static final String UPDATE_MSG_URL = BASE_URL + "app/cjst/version";//更新信息
+    public static final String AD_CONFIG_URL = BASE_URL + "app/cjst/config";//广告信息
 //    public static final String NEWS_AD_URL = BASE_URL + "pkx/ywSlide";//要闻头部广告
     public static final String VIDEO_DATA_URL_TITLE = BASE_URL +"cjst/nav";//&markid=476
     public static final String VIDEO_DATA_URL_ITEM = BASE_URL +"cjst/list_video";//&markid=476
